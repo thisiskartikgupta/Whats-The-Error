44838aaf952b26e55d2faa138501f6369535b14c
Kartik Gupta <thisiskartikgupta@gmail.com>
2020-12-27 16:24:33 +0530

    Filtered out error message

--- Whats-The-Error/main.py
+++ Whats-The-Error/main.py
@@ -2,9 +2,7 @@
 from requests import get
 from subprocess import Popen, PIPE
 
-
 '''
--1. Take input of file and assess its extension and respond appropriately.
 2. Run the subprocess and fetch the error message.
 3. Take the error fetched and pass it on to the StackExchange API.
 4. Retrieve result and show them appropriately.
@@ -18,28 +16,40 @@ print("______________________________________")
 print(" Welcome to Whats-The-Error Client ...")
 print("______________________________________")
 
-checkFileName = input().lower()
+# checkFileName = input().lower()
+checkFileName = "check.py"
 
 temp = checkFileName.split(".")
 extensionName = temp[len(temp) - 1]
 
 languages = ["py"]
 
-if extensionName in languages:
-    print("We support this file format !!")
-
-else:
+if extensionName not in languages:
     print("<ERROR : FormatError>> Our client never saw such file format. Make sure it has an extension")
+    exit()
 
 print("______________________________________")
 
-ProgramExecutionProcess = Popen(["python","checkFileName"], stdout=PIPE, stderr=PIPE)
+ProgramExecutionProcess = Popen(["python", checkFileName], stdout=PIPE, stderr=PIPE)
+
 stdout, stderr = ProgramExecutionProcess.communicate()
 
+# print("Program Output : " + stdout.decode("UTF-8"))
+# print("Program Error  : " + stderr.decode("UTF-8"))
 
-# baseURL = "https://api.stackexchange.com"
-# searchURL = "/2.2/search"
-#
+errorText = stderr.decode("UTF-8")
+
+errorClass = errorText[ errorText[: errorText.rfind("Error")].rfind(" "): errorText.rfind("Error")].split()[1]
+errorClass = errorClass + str("Error")
+
+errorMessage = errorText[ errorText.rfind(errorClass) :]
+
+print("Error Type        : " + errorClass)
+print("Error Description : " + errorMessage)
+
+#baseURL = "https://api.stackexchange.com"
+#searchURL = "/2.2/search"
+
 # query = {"order": "desc", "sort": "activity", "tagged": "Python", "intitle": "TypeError", "site": "stackoverflow"}
 #
 # getDataRequest = requests.get(f"{baseURL}{searchURL}", params=query)
